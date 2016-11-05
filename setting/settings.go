143b407d2ccf4434923ba01dfa76e4157a541886
mbBigDataTeam <bill.hu@microbenefit.com>
2016-11-05 09:54:43 +0800

    comit

--- setting/settings.go
+++ setting/settings.go
@@ -17,6 +17,7 @@ var (
 	ES_Cluster_Domains string
 	Limit int
 	Top int
+	SignUrl string
 )
 
 // initialize config file information
@@ -42,6 +43,7 @@ func init()  {
 	DataQuery, err = config.GetString("data_query", "") //prod 10.24.35.212:7777 -- dev 192.168.174.139:8085
 	Limit,err = config.GetInt("limit", "10000")
 	Top, err = config.GetInt("top",2000)
+	SignUrl, err = config.GetString("signUrl","http://databi.ifuli.cn:39200")
 	ES_Index, err = config.GetString("elastic_search/index","")
 	ES_Host,err = config.GetString("elastic_search/host","")
 	ES_Port,err = config.GetString("elastic_search/port","")
